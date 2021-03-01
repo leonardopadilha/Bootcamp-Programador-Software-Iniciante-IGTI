265cd73b59eb3b4e53b87f26830a18489d2fffd8
leonardopadilha <leonardopadilha28@gmail.com>
2021-03-01 13:16:36 -0300

    numerosPrimos

---
+++ numerosPrimos.js
@@ -0,0 +1,17 @@
+var num = parseInt(process.argv[2]);
+
+    if (num > 1){
+        if ((num > 2) && (num % 2 == 0)){
+            console.log("O número " + num + " não é primo");
+        }else if ((num > 3) && (num % 3 == 0)){
+            console.log("O número " + num + " não é primo");
+        }else if ((num > 5) && (num % 5 == 0)){
+            console.log("O número " + num + " não é primo");
+        }else if ((num > 7) && (num % 7 == 0)){
+            console.log("O número " + num + " não é primo");
+        }else {
+            console.log("O número " + num + " é primo");
+        }
+    }else {
+        console.log("O número " + num + " não é primo");
+    }
